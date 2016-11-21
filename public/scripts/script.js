731c7718ccaedab51e0b0e973f1b019db148a838
Aléxia Balionis <balionisalexia@gmail.com>
2016-11-21 17:39:13 -0200

    Updates

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -168,99 +168,103 @@
 
     });
 
-    $("#translateBtn").click( function () {
-      var x1 = $('#x1').val(),
-          y1 = $('#y1').val(),
-          x2 = $('#x2').val(),
-          y2 = $('#y2').val(),
-          r = $("#r").val(),
-          color = $("#colorpick").val();
+    // $("#translateBtn").click( function () {
+    //   var x1 = $('#x1').val(),
+    //       y1 = $('#y1').val(),
+    //       x2 = $('#x2').val(),
+    //       y2 = $('#y2').val(),
+    //       r = $("#r").val(),
+    //       color = $("#colorpick").val();
+    //
+    //   var val = $(".objtype option:selected").index();
+    //   var translateX = $("#translateX").val(),
+    //       translateY = $("#translateY");
+    //
+    //   if (val === 1) {
+    //     if (val && x1 && y1 && r) {
+    //       ctx.beginPath();
+    //       ctx.fillStyle = color;
+    //       ctx.strokeStyle = color;
+    //       ctx.arc(x1, y1, r, 0, Math.PI*2, true);
+    //       ctx.moveTo(translateX, translateY);
+    //       ctx.arc(x1, y1, r, 0, Math.PI*2, true);
+    //       ctx.stroke();
+    //       ctx.fill();
+    //       console.log("Circulo desenhado");
+    //     }
+    //   } else if (val === 2) {
+    //     if (x1 && x2 && y1 && y2) {
+    //       ctx.beginPath();
+    //       ctx.fillStyle= color;
+    //       ctx.fillRect(x1 , y1, x2, y2);
+    //       ctx.translate(translateX, translateY);
+    //       ctx.fillRect(translateX , translateX, x2, y2);
+    //       ctx.stroke();
+    //       console.log("click");
+    //     };
+    //   } else if (val === 3) {
+    //     if (x1 && x2 && y1 && y2) {
+    //       ctx.beginPath();
+    //       ctx.strokeStyle= color;
+    //     //   ctx.translate(translateX, translateY);
+    //       ctx.moveTo(x1, y1);
+    //       ctx.lineTo(translateX, translateY);
+    //       ctx.stroke();
+    //     };
+    //   };
+    //
+    // });
 
-      var val = $(".objtype option:selected").index();
-      var translateX = $("#translateX").val(),
-          translateY = $("#translateY");
-
-      if (val === 1) {
-        if (val && x1 && y1 && r) {
-          ctx.beginPath();
-          ctx.fillStyle = color;
-          ctx.strokeStyle = color;
-          ctx.arc(x1, y1, r, 0, Math.PI*2, true);
-          ctx.moveTo(translateX, translateY);
-          ctx.arc(x1, y1, r, 0, Math.PI*2, true);
-          ctx.stroke();
-          ctx.fill();
-          console.log("Circulo desenhado");
-        }
-      } else if (val === 2) {
-        if (x1 && x2 && y1 && y2) {
-          ctx.beginPath();
-          ctx.fillStyle= color;
-          ctx.fillRect(x1 , y1, x2, y2);
-          ctx.translate(translateX, translateY);
-          ctx.fillRect(translateX , translateX, x2, y2);
-          ctx.stroke();
-          console.log("click");
-        };
-      } else if (val === 3) {
-        if (x1 && x2 && y1 && y2) {
-          ctx.beginPath();
-          ctx.strokeStyle= color;
-        //   ctx.translate(translateX, translateY);
-          ctx.moveTo(x1, y1);
-          ctx.lineTo(translateX, translateY);
-          ctx.stroke();
-        };
-      };
-
-    });
-
-    $("#transformBtn").click( function () {
-      var x1 = $('#x1').val(),
-          y1 = $('#y1').val(),
-          x2 = $('#x2').val(),
-          y2 = $('#y2').val(),
-          r = $("#r").val(),
-          color = $("#colorpick").val();
-
-      var val = $(".objtype option:selected").index();
-      var translateX = $("#translateX").val(),
-          translateY = $("#translateY");
-
-      if (val === 1) {
-        if (val && x1 && y1 && r) {
-          ctx.beginPath();
-          ctx.fillStyle = color;
-          ctx.strokeStyle = color;
-          ctx.arc(x1, y1, r, 0, Math.PI*2, true);
-          ctx.moveTo(translateX, translateY);
-          ctx.arc(x1, y1, r, 0, Math.PI*2, true);
-          ctx.stroke();
-          ctx.fill();
-          console.log("Circulo desenhado");
-        }
-      } else if (val === 2) {
-        if (x1 && x2 && y1 && y2) {
-          ctx.beginPath();
-          ctx.fillStyle= color;
-          ctx.fillRect(x1 , y1, x2, y2);
-          ctx.translate(translateX, translateY);
-          ctx.fillRect(translateX , translateX, x2, y2);
-          ctx.stroke();
-          console.log("click");
-        };
-      } else if (val === 3) {
-        if (x1 && x2 && y1 && y2) {
-          ctx.beginPath();
-          ctx.strokeStyle= color;
-          ctx.translate(translateX, translateY);
-          ctx.moveTo(x1, y1);
-          ctx.lineTo(translateX, translateY);
-          ctx.stroke();
-        };
-      };
-
-    });
+    // $("#transformBtn").click( function () {
+    //   var x1 = $('#x1').val(),
+    //       y1 = $('#y1').val(),
+    //       x2 = $('#x2').val(),
+    //       y2 = $('#y2').val(),
+    //       r = $("#r").val(),
+    //       color = $("#colorpick").val();
+    //
+    //   var val = $(".objtype option:selected").index();
+    //   var a = $("#transformA").val(),
+    //       b = $("#transformB").val(),
+    //       c = $("#transformC").val(),
+    //       d = $("#transformD").val(),
+    //       e = $("#transformE").val(),
+    //       f = $("#transformF").val();
+    //
+    //   if (val === 1) {
+    //     if (val && x1 && y1 && r) {
+    //       ctx.beginPath();
+    //       ctx.fillStyle = color;
+    //       ctx.strokeStyle = color;
+    //       ctx.arc(x1, y1, r, 0, Math.PI*2, true);
+    //       ctx.transform(a,b,c,d,e,f);
+    //       ctx.arc(x1, y1, r, 0, Math.PI*2, true);
+    //       ctx.stroke();
+    //       ctx.fill();
+    //       console.log("Circulo desenhado");
+    //     }
+    //   } else if (val === 2) {
+    //     if (x1 && x2 && y1 && y2) {
+    //       ctx.beginPath();
+    //       ctx.fillStyle= color;
+    //       ctx.fillRect(x1 , y1, x2, y2);
+    //       ctx.transform(a,b,c,d,e,f);
+    //       ctx.fillRect(x1, y1, x2, y2);
+    //       ctx.stroke();
+    //       console.log("click");
+    //     };
+    //   } else if (val === 3) {
+    //     if (x1 && x2 && y1 && y2) {
+    //       ctx.beginPath();
+    //       ctx.strokeStyle= color;
+    //       ctx.transform(a,b,c,d,e,f);
+    //       ctx.moveTo(x1, y1);
+    //       ctx.lineTo(translateX, translateY);
+    //       ctx.stroke();
+    //     };
+    //   };
+    //
+    // });
 
 
 
